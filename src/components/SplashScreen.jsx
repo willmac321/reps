f99dda711c3d40d72aa0c652ca69ff5a45dc4b34
WM <will@loblollysoftware.com>
2021-01-01 16:23:06 -0600

    updates

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Card, Title, Paragraph } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { withTheme } from 'react-native-paper';
+import Adonis from '~/src/components/Adonis.jsx';
 
-const SplashScreen = () => (
-  <Card>
-    <Card.Content>
-      <Title>test</Title>
-      <Paragraph>content</Paragraph>
-    </Card.Content>
-  </Card>
+const styles = StyleSheet.create({
+  img: {
+    marginTop: '10%',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    width: '30%',
+    height: '30%',
+    display: 'block',
+  },
+  parentView: {
+    height: '100%',
+    width: '100%',
+  },
+});
+
+const SplashScreen = ({ theme }) => (
+  <View style={[styles.parentView, styles.container]}>
+    <Adonis style={StyleSheet.flatten([styles.img, { fill: theme.colors.primary }])} />
+  </View>
 );
 
-export default SplashScreen;
+export default withTheme(SplashScreen);
