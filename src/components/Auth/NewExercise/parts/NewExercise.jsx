74a86d0fab087a13e732001c3695de61fd5a8d4c
WM <will@loblollysoftware.com>
2021-01-29 18:01:26 -0600

    update new exercises

--- src/components/Auth/NewExercise/parts/NewExercise.jsx
+++ src/components/Auth/NewExercise/parts/NewExercise.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, View, StyleSheet } from 'react-native';
 import { withTheme, TextInput, HelperText } from 'react-native-paper';
 import CardWithButton from '../../../../template/CardWithButton';
 
-const NewWorkout = ({ navigation, theme, data }) => {
+const NewWorkout = ({ navigation, theme, exercises, setExercises, workout }) => {
   const [isDisable, setIsDisable] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
-  const [workoutName, setWorkoutName] = React.useState('');
+  const [newExercise, setNewExercise] = React.useState({
+    id: '',
+    title: '',
+    sets: 0,
+    repRange: [0, 0],
+    rest: 0,
+  });
+
   const styles = StyleSheet.create({
     input: theme.input,
   });
 
-  const isNameTaken = () => data.map((a) => a.title).includes(workoutName);
+  const isNameTaken = () => exercises.map((a) => a.title).includes(newExercise.name);
+  const isEmpty = () =>
+    !(
+      newExercise.id &&
+      newExercise.title &&
+      newExercise.sets &&
+      newExercise.rest &&
+      newExercise.repRange[1]
+    );
 
   React.useEffect(() => {
-    if (!workoutName || isNameTaken()) {
+    if (isEmpty() || isNameTaken()) {
       setIsDisable(true);
     } else {
       setIsDisable(false);
     }
-  }, [workoutName, data]);
+  }, [workout, exercises]);
 
   const handleOnPress = () => {
     setIsLoading(true);
@@ -30,10 +45,10 @@ const NewWorkout = ({ navigation, theme, data }) => {
   };
 
   return (
-    <>
+    <KeyboardAvoidingView>
       <CardWithButton
-        title="Name your workout"
-        buttonText="Ok"
+        title="Build-a-Exercise"
+        buttonText="Add"
         showButton
         buttonDisabled={isDisable}
         flex={1}
@@ -49,8 +64,8 @@ const NewWorkout = ({ navigation, theme, data }) => {
           textContentType="emailAddress"
           label="Workout name"
           error={isError}
-          value={workoutName}
-          onChangeText={(val) => setWorkoutName(val)}
+          value={newExercise.title}
+          onChangeText={(val) => setNewExercise(() => ({ ...newExercise, title: val }))}
           style={styles.input}
         />
         {!!isNameTaken() && (
@@ -58,8 +73,26 @@ const NewWorkout = ({ navigation, theme, data }) => {
             Name is already taken!
           </HelperText>
         )}
+        <View>
+          <TextInput
+            mode="outlined"
+            theme={theme}
+            autoCompleteType="email"
+            textContentType="emailAddress"
+            label="Workout name"
+            error={isError}
+            value={newExercise.title}
+            onChangeText={(val) => setNewExercise(() => ({ ...newExercise, title: val }))}
+            style={styles.input}
+          />
+          {!!isNameTaken() && (
+            <HelperText type="error" visible={isNameTaken()}>
+              Name is already taken!
+            </HelperText>
+          )}
+        </View>
       </CardWithButton>
-    </>
+    </KeyboardAvoidingView>
   );
 };
 export default withTheme(NewWorkout);
