2c30536decc2c11fcbb3bd3c601e29351df8e0b8
WM <will@loblollysoftware.com>
2024-01-15 18:35:59 -0500

    confetti now?

--- src/common/Celebration.jsx
+++ src/common/Celebration.jsx
@@ -97,11 +97,9 @@ const Celebration = ({ style, theme, isVisible, setIsVisible }) => {
     <Portal>
       <View
         style={{
-          left: 0,
-          right: 0,
-          position: "absolute",
-          top: 0,
-          bottom: 0,
+          width: "100%",
+          height: "100%",
+          flex: 1,
           overflow: "hidden",
         }}
       >
